d2b8341f382bc71b59dc61d364241ff77ef49308
Hector Pugh <hectorpugh08@icloud.com>
2025-05-25 03:16:45 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-<head>
-<title>
-Duke's Portfolio.
-</title>
-</head>  
     <body className="antialiased">
         <Main />
         <NextScript />
