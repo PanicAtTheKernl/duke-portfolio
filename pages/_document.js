449a34ae2e0c9030a7ced471ae5f5c21a3242173
Hector Pugh <hectorpugh08@icloud.com>
2025-07-03 21:57:51 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>
-          Coded By Duke.
+          CodedByDuke | IT Solutions.
         </title>
       </Head>
       <body className="antialiased">
