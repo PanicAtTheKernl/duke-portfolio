f8aa3392d73e42ad5d4339034d7f9abae96bd3b5
Hector Pugh <hectorpugh08@icloud.com>
2025-07-23 20:00:33 +0100

    modified:   pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,9 @@ import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div style={{
-      minHeight: '100vh',
-      background: '#f4f6f8',
-      fontFamily: 'Inter, Arial, sans-serif',
-      color: '#222',
-    }}>
+    <>
       <Component {...pageProps} />
       <Analytics />
-    </div>
+    </>
   );
 }
